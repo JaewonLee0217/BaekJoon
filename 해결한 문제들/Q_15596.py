a70b66558e96fb564c2690a0b70458c0ff6f0edc
jaewon Lee <jcircle67@gmail.com>
2020-07-23 17:43:17 +0900

    baekjoon ans2
    baekjoon ans2

---
+++ 해결한 문제들/Q_15596.py
@@ -0,0 +1,8 @@
+#정수 N개의 합
+
+def solve(a):
+    ans = 0
+    for i in a:
+        ans += i
+    return ans
+
---
+++ 해결한 문제들/Q_2739.py
@@ -0,0 +1,8 @@
+#원하는 구구단 출력
+N = int(input())
+for i in range(1,10):
+    print("%d*%d=%d"%(N,i,N*i))
+
+
+
+        
