8ec2084e27193144bccc2ee117d1f44171ec640b
jaewon Lee <jcircle67@gmail.com>
2020-07-25 01:03:20 +0900

    Update Q_1316(그룹 단어 체커).py

--- 알고리즘/문자열/Q_1316(그룹 단어 체커).py
+++ 알고리즘/문자열/Q_1316(그룹 단어 체커).py
@@ -4,4 +4,21 @@
 #둘째 줄부터 N개의 줄에 단어가 들어온다. 단어는 알파벳 소문자로만 되어있고 중복되지 않으며, 길이는 최대 100이다.
 
 #Output : 첫째 줄에 그룹 단어의 개수를 출력한다.
+#그 string을 어떻게 구분해서 읽을까.
 
+num = int(input())
+
+count =0
+for i in range(num):
+    str = input()
+    for a in range(len(str)):
+        if a != len(str)-1:
+            if str[a]==str[a+1]:
+                pass
+            elif str[a] in str[a+1:]:
+                break
+        else:
+            count+=1
+print(count)
+        
+    
