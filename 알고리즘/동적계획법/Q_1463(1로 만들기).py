755e85346922969b663b9f6de5bb3f7f9d318bfe
jaewon Lee <jcircle67@gmail.com>
2020-08-02 16:30:10 +0900

    Update Q_1463(1로 만들기).py

--- 알고리즘/동적계획법/Q_1463(1로 만들기).py
+++ 알고리즘/동적계획법/Q_1463(1로 만들기).py
@@ -3,19 +3,14 @@
 #2.X가 2로 나누어 떨어지면, 2로 나눈다.
 #3.1을 뺀다.
 #정수 한 개를 입력 받아 1로 만들고 연산을 사용한 횟수를 출력.
-
 N = int(input())
-
 arr = [0]*(N+1)
-
 for i in range(2,N+1):
     arr[i] = arr[i-1]+1
     if i%2==0 and arr[i]>arr[i//2]+1:
         arr[i]=arr[i//2]+1
     if i%3==0 and arr[i]>arr[i//3]+1:
         arr[i]=arr[i//3]+1
-    
-
 print(arr[N])
         
         
