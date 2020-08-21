762c06fedcd92b8b691fe6c41f20eb41ee891247
jaewon Lee <jcircle67@gmail.com>
2020-08-21 11:22:15 +0900

    Update Q_2309(일곱 난쟁이).py
    Q_2309 completed

--- 알고리즘/BruteForce/Q_2309(일곱 난쟁이).py
+++ 알고리즘/BruteForce/Q_2309(일곱 난쟁이).py
@@ -3,14 +3,18 @@ k = []
 for i in range(9):
     t = int(sys.stdin.readline())
     k.append(t)
-ksum = sum(k)
+ksum = sum(k)-100
 for i in range(0,8):
-    for j in range(1,9):
-        if (ksum-k[i]-k[j]) ==100:
-            k.remove(k[i])
-            k.remove(k[j])
-            k.sort()
+    for j in range(i+1,9):
+        if ksum ==(k[i]+k[j]):
+            a=k[i]
+            b=k[j]
             break
+
+
+k.remove(a)
+k.remove(b)
+k.sort()
 for i in range(len(k)):
     print(k[i])
             
