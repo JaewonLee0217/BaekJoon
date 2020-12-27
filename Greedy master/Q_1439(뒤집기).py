35246e0b1da36faf3e708e31e79ca3e5ee6e6b68
jaewon Lee <jcircle67@gmail.com>
2020-12-27 15:32:07 +0900

    Create Q_1439(뒤집기).py
    success

---
+++ Greedy master/Q_1439(뒤집기).py
@@ -0,0 +1,32 @@
+import sys
+#input = sys.stdin.readline
+
+n = list(input())
+prev = n[0]
+result= []
+count = 0
+# 0번 인덱스 기준
+for i in n:
+    if i != prev and i != n[0]:
+        count += 1
+    prev = i
+result.append(count)
+
+# 0번 인덱스 기준으로 뒤짚기
+count = 0
+for i in n:
+    if i =='0':
+        i =='1'
+    else:
+        i == '0'
+prev = n[0]
+count +=1
+for i in n:
+    if i != prev and i != n[0]:
+        count += 1
+    prev = i
+result.append(count)
+result.sort()
+print(result[0])
+
+    
