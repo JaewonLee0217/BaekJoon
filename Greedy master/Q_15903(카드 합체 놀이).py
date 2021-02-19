e49c8fe61fa05eb6b2334762f5225aee044b7415
jaewon Lee <jcircle67@gmail.com>
2021-02-19 18:14:32 +0900

    Create Q_15903(카드 합체 놀이).py

---
+++ Greedy master/Q_15903(카드 합체 놀이).py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+a,b = map(int, input().split())
+card  = list(map(int,input().split()))
+
+for i in range(b):
+    card = sorted(card)
+    cover = card[0] + card[1]
+    card[0] = cover
+    card[1] = cover
+
+print(sum(card))
